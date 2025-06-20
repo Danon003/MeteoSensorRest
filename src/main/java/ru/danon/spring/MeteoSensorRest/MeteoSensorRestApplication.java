fa70a42d084d1ddf42530c0dc56871406e8a2ff4
Danon003 <danielvigerin@yandex.ru>
2025-06-20 16:55:09 +0300

    Initial commit

---
+++ src/main/java/ru/danon/spring/MeteoSensorRest/MeteoSensorRestApplication.java
@@ -0,0 +1,13 @@
+package ru.danon.spring.MeteoSensorRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteoSensorRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeteoSensorRestApplication.class, args);
+	}
+
+}
